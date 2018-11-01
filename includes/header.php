7dedbc683e74b76ebf0c83836d301cdcb8fec32b
Matt Strand <mstrandwest@gmail.com>
2018-10-31 22:48:44 -0400

    Add files via upload
    This version isn't as good as it was in php, but Github doesn't support php with hosting

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Matt Strand</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="personalSite.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+</head>
+
+<body>
+	<div class="navContainer">
+		<nav class="navbar fixed-top">
+		 	<a href="index.php"><img src="resources/CUlogo.png"></a>
+		 	<a href="index.php"><button id="navButton">About Me</button></a>
+		 	<button id="navButton" onclick=" window.open('resources/resume.pdf','_blank')">Resume PDF</button>
+		 	<a href="resume.php"><button id="navButton">Resume Online</button></a>
+		 	<a href="otherworks.php"><button id="navButton">My Other Works</button></a>
+		</nav>
+	</div>	
+	<hr style="max-width: 60%;">
+</body>
+
+<!-- Bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
